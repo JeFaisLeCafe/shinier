9909d71897b0825b49fda2fed2dac77bd3d08ce6
Pierre-Etienne Soury <pierretienne69@gmail.com>
2018-05-28 16:54:04 +0200

    modified accordingly to Kar's will

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :questions, except: [ :destroy, :edit, :update ]
+  resources :questions
   resources :answers
-  resources :diseases, only: [ :index, :show, :new, :create, :edit, :update ]
+  resources :diseases, except: [ :destroy ]
 
 end
