6550bf75a2e00ae4f65d0183863b82232b802228
Karl Raf <karlforbusiness@gmail.com>
2018-05-28 18:12:45 +0200

    crud answer look ok

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,53 @@
+class AnswersController < ApplicationController
+
+  def new
+    @user = current_user
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new(answer_params)
+    @answer.user = current_user
+    @answer.question = @question
+    if @answer.save
+      #redirect somewhere
+    else
+      render :new
+    end
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+  end
+
+  def edit
+    @answer = Answer.find(params[:id])
+    @question = Question.find(params[:question_id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+    @answer.update(answer_params)
+    if @answer.save
+      #redirect somewhere
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id].to_i)
+    @answer.destroy
+    redirect_to question_answers_path
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body, :user_id, :question_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :questions
-  resources :answers
+  resources :questions do
+    resources :answers
+  endr
   resources :diseases, except: [ :destroy ]
 
 end
