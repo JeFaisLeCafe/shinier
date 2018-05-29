b97f6da958e0616a346c89cb2dbd6ad71da3aff3
Karl Raf <karlforbusiness@gmail.com>
2018-05-29 15:23:50 +0200

    we can upvote and question will be mark as useful

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,20 @@ class Question < ApplicationRecord
   validates :body, presence: true
   validates :user, presence: true
   acts_as_votable
+
+
+
+# if we have more than 5 upvotes in the answers of the question, it will mark as useful
+  def useful?
+    answers = []
+    Question.last.answers.each do |answer|
+      answers << answer.get_upvotes.size
+    end
+    if answers.sum > 5
+      return true
+    else
+      return false
+    end
+  end
+
 end
