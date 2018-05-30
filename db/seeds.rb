bb62be536ad3bea40cd83ae31905f031e2bef320
Pierre-Etienne Soury <pierretienne69@gmail.com>
2018-05-30 11:04:46 +0200

    added some answers

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,19 @@ end
   puts "#{disease.name} created!"
 
 10.times do
-  user = User.create!(firstname: Faker::Cat.name, lastname: Faker::OnePiece.island, email: Faker::Internet.email, password: "secret", disease: disease, location: Faker::Address.city, status_in_community: "Newbie", bio: Faker::Lorem.paragraph, secu_nb: "1 23 04 69 666 666 11" )
+  user = User.create!(firstname: Faker::Cat.name, lastname: Faker::OnePiece.island, email: Faker::Internet.unique.email, password: "secret", disease: disease, location: Faker::Address.city, status_in_community: "Newbie", bio: Faker::Lorem.paragraph, secu_nb: "1 23 04 69 666 666 11" )
   puts "#{user.firstname} created!"
 
-  user2 = User.create!(firstname: Faker::Cat.name, lastname: Faker::OnePiece.island, email: Faker::Internet.email, password: "secret", disease: disease, location: Faker::Address.city, status_in_community: "Newbie", bio: Faker::Lorem.paragraph, secu_nb: "1 23 04 69 666 666 11" )
+  user2 = User.create!(firstname: Faker::Cat.name, lastname: Faker::OnePiece.island, email: Faker::Internet.unique.email, password: "secret", disease: disease, location: Faker::Address.city, status_in_community: "Newbie", bio: Faker::Lorem.paragraph, secu_nb: "1 23 04 69 666 666 11" )
   puts "#{user2.firstname} created!"
 
   question = Question.create!(title: "#{Faker::GameOfThrones.character} x #{Faker::GameOfThrones.character} real?", body: Faker::GameOfThrones.quote, disease: disease, user: user)
   puts "#{question.title} created!"
 
-  answer = Answer.create!(body: Faker::Hipster.paragraph, upvotes: rand(*[0..42]), question: question, user: user2)
-  puts "answer created!"
+  rand(*[2..12]).times do
+    answer = Answer.create!(body: Faker::Hipster.paragraph, upvotes: rand(*[0..42]), question: question, user: user2)
+  end
+  puts "answers created!"
 
   puts "Seed ok!"
 end
