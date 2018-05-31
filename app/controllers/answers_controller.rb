3e094e6a15e9a4f77464b7bfb365e3e3e8632b2a
Karl Raf <karlforbusiness@gmail.com>
2018-05-31 15:49:51 +0200

    ajax question show

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -19,9 +19,15 @@ class AnswersController < ApplicationController
     @answer.question = @question
     authorize @answer
     if @answer.save
-      redirect_to question_path(@question)
+      respond_to do |format|
+        format.html { redirect_to question_path(@question) }
+        format.js
+      end
     else
-      render :new
+      respond_to do |format|
+        format.html { render :new }
+        format.js
+      end
     end
   end
 
