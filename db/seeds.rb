163acac11b24e130dda362e42f7749fb071ed542
Patricia Bernasconi <bernasconi.patricia@gmail.com>
2018-06-01 16:49:09 +0200

    seed ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,10 +19,11 @@ end
 
 # CREATING 10 USERS
 
+  firstname1 = "Tiago"
   user1 = User.create!(
-    firstname: Faker::Cat.name,
+    firstname: firstname1,
     lastname: Faker::OnePiece.island,
-    email: Faker::Internet.unique.email,
+    email: "#{firstname1}@gmail.com",
     password: "secret",
     disease: disease,
     location: Faker::Address.city,
@@ -32,10 +33,11 @@ end
   )
   puts "#{user.firstname} created!"
 
+  firstname2 = "Marc"
   user2 = User.create!(
-    firstname: Faker::Cat.name,
+    firstname: firstname2,
     lastname: Faker::OnePiece.island,
-    email: Faker::Internet.unique.email,
+    email: "#{firstname2}@gmail.com",
     password: "secret",
     disease: disease,
     location: Faker::Address.city,
@@ -45,11 +47,11 @@ end
   )
   puts "#{user.firstname} created!"
 
-
+  firstname3 = "Laurent"
   user3 = User.create!(
-    firstname: Faker::Cat.name,
+    firstname: firstname3,
     lastname: Faker::OnePiece.island,
-    email: Faker::Internet.unique.email,
+    email: "#{firstname3}@gmail.com",
     password: "secret",
     disease: disease,
     location: Faker::Address.city,
@@ -59,10 +61,11 @@ end
   )
   puts "#{user.firstname} created!"
 
+  firstname4 = "Thibault"
   user4 = User.create!(
-    firstname: Faker::Cat.name,
+    firstname: firstname4,
     lastname: Faker::OnePiece.island,
-    email: Faker::Internet.unique.email,
+    email: "#{firstname4}@gmail.com",
     password: "secret",
     disease: disease,
     location: Faker::Address.city,
@@ -72,6 +75,92 @@ end
   )
   puts "#{user.firstname} created!"
 
+  firstname5 = "Hervé"
+  user5 = User.create!(
+    firstname: firstname5,
+    lastname: Faker::OnePiece.island,
+    email: "#{firstname5}@gmail.com",
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
+  puts "#{user.firstname} created!"
+
+  firstname6 = "Patricia"
+  user6 = User.create!(
+    firstname: firstname6,
+    lastname: Faker::OnePiece.island,
+    email: "#{firstname6}@gmail.com",
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
+  puts "#{user.firstname} created!"
+
+  firstname7 = "Solange"
+  user7 = User.create!(
+    firstname: firstname7,
+    lastname: Faker::OnePiece.island,
+    email: "#{firstname7}@gmail.com",
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
+  puts "#{user.firstname} created!"
+
+  firstname8 = "Etienne"
+  user8 = User.create!(
+    firstname: firstname8,
+    lastname: Faker::OnePiece.island,
+    email: "#{firstname8}@gmail.com",
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
+  puts "#{user.firstname} created!"
+
+  firstname9 = "Tom"
+  user9 = User.create!(
+    firstname: firstname9,
+    lastname: Faker::OnePiece.island,
+    email: "#{firstname9}@gmail.com",
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
+  puts "#{user.firstname} created!"
+
+  firstname10 = "Karl"
+  user10 = User.create!(
+    firstname: firstname10,
+    lastname: Faker::OnePiece.island,
+    email: "#{firstname10}@gmail.com",
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
+  puts "#{user.firstname} created!"
+
+
+# CREATING 10 QUESTIONS
 
 
   question1 = Question.create!(
@@ -90,7 +179,74 @@ end
   )
   puts "#{question.title} created!"
 
-  answer_2_1 = Answer.create!(
+  question3 = Question.create!(
+    title: "Diabète et alcool, danger?",
+    body: "Vous prenez quoi comme drinks quand vous sortez? ",
+    disease: disease,
+    user: user3
+  )
+  puts "#{question.title} created!"
+
+  question4 = Question.create!(
+    title: "Journée mondiale du diabète : comment y participer ?",
+    body: " J’ai entendu parlé de cette journée dernièrement; des infos?",
+    disease: disease,
+    user: user5
+  )
+  puts "#{question.title} created!"
+
+  question5 = Question.create!(
+    title: "Connaissez-vous le lecteur de glycémie Freestyle Optium Neo ?",
+    body: " Il est parfois dur de s’y retrouver parmi tout les lecteurs, que pensez-vous de celui-ci?",
+    disease: disease,
+    user: user7
+  )
+  puts "#{question.title} created!"
+
+  question6 = Question.create!(
+    title: "Des noms de gynéco pro du diabète 1 sur Paris?",
+    body: "Je suis nouvelle en ville du coup je cherche le bon doc!",
+    disease: disease,
+    user: user7
+  )
+  puts "#{question.title} created!"
+
+  question7 = Question.create!(
+    title: "Pillule + Diabète?",
+    body: "Vous prenez quoi comme pillule?",
+    disease: disease,
+    user: user6
+  )
+  puts "#{question.title} created!"
+
+  question8 = Question.create!(
+    title: "Mains sèches et Humalog",
+    body: "Je galère à trouver des bonnes crèmes pour les mains avec Humalog",
+    disease: disease,
+    user: user10
+  )
+  puts "#{question.title} created!"
+
+  question9 = Question.create!(
+    title: "Insulines et lieu public..",
+    body: "Des tips pour être discret? Ca fait que 2 mois que j'ai commencé... ",
+    disease: disease,
+    user: user9
+  )
+  puts "#{question.title} created!"
+
+  question10 = Question.create!(
+    title: "Est-il possible de vivre sans insuline avec le diabète?",
+    body: "J’ai entendu parler de ça au JT l’autre soir, quelqu’un aurait-il des informations complémentaires?",
+    disease: disease,
+    user: user8
+  )
+  puts "#{question.title} created!"
+
+
+# CREATING 20 ANSWERS
+
+  answer1_2_1 = Answer.create!(
       body: "Quelle belle avancée !! Reste à mieux identifier la source de ce basculement qui nous fait passer de l'auto-immun bénin à DT1.",
       upvotes: rand(*[0..42]),
       question: question2,
@@ -98,14 +254,77 @@ end
     )
   puts "answers created!"
 
-  answer_2_1 = Answer.create!(
-      body: "Quelle belle avancée !! Reste à mieux identifier la source de ce basculement qui nous fait passer de l'auto-immun bénin à DT1.",
+  answer2_2_3 = Answer.create!(
+      body: "Je vais suivre ça!",
       upvotes: rand(*[0..42]),
       question: question2,
       user: user3
     )
   puts "answers created!"
 
+  answer3_1_1 = Answer.create!(
+      body: "Quelle belle histoire! ",
+      upvotes: rand(*[0..42]),
+      question: question1,
+      user: user1
+    )
+  puts "answers created!"
+
+  answer4_4_3 = Answer.create!(
+      body: "Elle a lieu tout les 14 novembre, tu peux t’inscrire sur le site de l’IDF (international Diabetes Federation)",
+      upvotes: rand(*[0..42]),
+      question: question4,
+      user: user3
+    )
+  puts "answers created!"
+
+  answer5_7_7 = Answer.create!(
+      body: "Je prends cerazette c'est parfait car synthèse d'hormones naturelles ! ",
+      upvotes: rand(*[0..42]),
+      question: question7,
+      user: user7
+    )
+  puts "answers created!"
+
+  answer6_7_6 = Answer.create!(
+      body: "Moi aussi je suis sur Cerazette! Tout à fait d'accord c'est super!",
+      upvotes: rand(*[0..42]),
+      question: question7,
+      user: user6
+    )
+  puts "answers created!"
+
+  answer7_9_3 = Answer.create!(
+      body: "J'essaie de faire ça à la maison... un peu triste",
+      upvotes: rand(*[0..42]),
+      question: question9,
+      user: user3
+    )
+  puts "answers created!"
+
+  answer8_9_5 = Answer.create!(
+      body: "Au pire au toilettes mais bon après il faut s'y habituer tu verras ça ira mieux bientôt :)",
+      upvotes: rand(*[0..42]),
+      question: question9,
+      user: user1
+    )
+  puts "answers created!"
+
+  answer8_ = Answer.create!(
+      body: "EAvène power!! définitivement les meilleures :)",
+      upvotes: rand(*[0..42]),
+      question: question4,
+      user: user7
+    )
+  puts "answers created!"
+
+  answer8_ = Answer.create!(
+      body: "La gamme Même est vraiment bien",
+      upvotes: rand(*[0..42]),
+      question: question4,
+      user: user9
+    )
+  puts "answers created!"
 
 end
 
