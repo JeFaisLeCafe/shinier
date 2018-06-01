bec82e142c227ba36898a56359e30683269b0afa
Patricia Bernasconi <bernasconi.patricia@gmail.com>
2018-06-01 15:37:57 +0200

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,104 @@ if Rails.env.development?
 end
 
 
-  a = Faker::GameOfThrones.house
-  disease = Disease.create!(name: a, symptomes: Faker::GameOfThrones.city, latin_name: "#{a}um")
+  a = "Diabete type 1"
+  disease = Disease.create!(
+    name: a,
+    symptomes: "une augmentation du besoin d’uriner ; une augmentation de la soif (polydipsie) ; une perte de poids malgré un appétit qui augmente ;
+                une fatigue importante ou une vision trouble",
+    latin_name: "#{a}um"
+  )
   puts "#{disease.name} created!"
 
-10.times do
-  user = User.create!(firstname: Faker::Cat.name, lastname: Faker::OnePiece.island, email: Faker::Internet.unique.email, password: "secret", disease: disease, location: Faker::Address.city, status_in_community: "Newbie", bio: Faker::Lorem.paragraph, secu_nb: "1 23 04 69 666 666 11" )
+# CREATING 10 USERS
+
+  user1 = User.create!(
+    firstname: Faker::Cat.name,
+    lastname: Faker::OnePiece.island,
+    email: Faker::Internet.unique.email,
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
   puts "#{user.firstname} created!"
 
-  user2 = User.create!(firstname: Faker::Cat.name, lastname: Faker::OnePiece.island, email: Faker::Internet.unique.email, password: "secret", disease: disease, location: Faker::Address.city, status_in_community: "Newbie", bio: Faker::Lorem.paragraph, secu_nb: "1 23 04 69 666 666 11" )
-  puts "#{user2.firstname} created!"
+  user2 = User.create!(
+    firstname: Faker::Cat.name,
+    lastname: Faker::OnePiece.island,
+    email: Faker::Internet.unique.email,
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
+  puts "#{user.firstname} created!"
 
-  question = Question.create!(title: "#{Faker::GameOfThrones.character} x #{Faker::GameOfThrones.character} real?", body: Faker::GameOfThrones.quote, disease: disease, user: user)
+
+  user3 = User.create!(
+    firstname: Faker::Cat.name,
+    lastname: Faker::OnePiece.island,
+    email: Faker::Internet.unique.email,
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
+  puts "#{user.firstname} created!"
+
+  user4 = User.create!(
+    firstname: Faker::Cat.name,
+    lastname: Faker::OnePiece.island,
+    email: Faker::Internet.unique.email,
+    password: "secret",
+    disease: disease,
+    location: Faker::Address.city,
+    status_in_community: "Newbie",
+    bio: Faker::Lorem.paragraph,
+    secu_nb: "1 23 04 69 666 666 11"
+  )
+  puts "#{user.firstname} created!"
+
+
+
+  question1 = Question.create!(
+    title: "L’incroyable défi d’Haka, ado diabétique de 13 ans",
+    body: "Son diabète de type 1 l’a plongé il y a deux ans dans le coma. Pas de quoi arrêter Haka. L’ado de 13 ans va parcourir 2 000 km en courant et à vélo, comme un pied de nez à sa maladie",
+    disease: disease,
+    user: user1
+  )
   puts "#{question.title} created!"
 
-  rand(*[2..12]).times do
-    answer = Answer.create!(body: Faker::Hipster.paragraph, upvotes: rand(*[0..42]), question: question, user: user2)
-  end
+  question2 = Question.create!(
+    title: "Le rôle du thymus n’est pas celui que l’on croyait !",
+    body: "C’est une petite révolution dans le monde de la recherche sur le diabète de type 1 : une étude menée par l’équipe Inserm dirigée par Roberto Mallone à l’Institut Cochin (Inserm, CNRS, Université Paris Descartes), remet en question le rôle attribué de longue date au thymus dans la sélection et l’élimination des globules blancs associés au diabète de type 1 et révèle que nous sommes tous auto-immuns.",
+    disease: disease,
+    user: user1
+  )
+  puts "#{question.title} created!"
+
+  answer_2_1 = Answer.create!(
+      body: "Quelle belle avancée !! Reste à mieux identifier la source de ce basculement qui nous fait passer de l'auto-immun bénin à DT1.",
+      upvotes: rand(*[0..42]),
+      question: question2,
+      user: user1
+    )
   puts "answers created!"
+
+  answer_2_1 = Answer.create!(
+      body: "Quelle belle avancée !! Reste à mieux identifier la source de ce basculement qui nous fait passer de l'auto-immun bénin à DT1.",
+      upvotes: rand(*[0..42]),
+      question: question2,
+      user: user3
+    )
+  puts "answers created!"
+
 
 end
 
