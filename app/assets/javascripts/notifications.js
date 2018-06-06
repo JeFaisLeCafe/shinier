313fd38197b1e1d14118c514995cc4c73e55598f
Karl Raf <karlforbusiness@gmail.com>
2018-06-06 09:33:17 +0200

    allo

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,30 +1,30 @@
-$(function() {
-  let Notifications;
-  return (Notifications = class Notifications {
-    constructor() {
-      this.handleSuccess = this.handleSuccess.bind(this);
-      this.getNewNotifications();
-      this.notifications = $("[data-behavior='notifications']");
+// $(function() {
+//   let Notifications;
+//   return (Notifications = class Notifications {
+//     constructor() {
+//       this.handleSuccess = this.handleSuccess.bind(this);
+//       this.getNewNotifications();
+//       this.notifications = $("[data-behavior='notifications']");
 
-      if (this.notifications.length > 0) {
-        this.handleSuccess(this.notifications.data('notifications'));
-      }
-    }
+//       if (this.notifications.length > 0) {
+//         this.handleSuccess(this.notifications.data('notifications'));
+//       }
+//     }
 
-    getNewNotifications() {
-      return $.ajax({
-        url: '/notifications.json',
-        dataType: 'JSON',
-        method: 'GET',
-        success: this.handleSuccess
-      });
-    }
+//     getNewNotifications() {
+//       return $.ajax({
+//         url: '/notifications.json',
+//         dataType: 'JSON',
+//         method: 'GET',
+//         success: this.handleSuccess
+//       });
+//     }
 
-    handleSuccess(data) {
-      const items = $.map(data, notification => notifications.template);
+//     handleSuccess(data) {
+//       const items = $.map(data, notification => notifications.template);
 
-      $("[data-behavior='unread-count']").text(items.length);
-      return $("[data-behavior='notification-items']").append(items);
-    }
-  });
-});
+//       $("[data-behavior='unread-count']").text(items.length);
+//       return $("[data-behavior='notification-items']").append(items);
+//     }
+//   });
+// });
