055798b622fadccd59758aad8af61a2e85fa0ce9
Karl Raf <karlforbusiness@gmail.com>
2018-06-14 16:05:28 +0200

    preparing infinit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -38,6 +38,10 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     @answer = Answer.new
     @answers = @question.answers.page(params[:page]).per(5)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def edit
