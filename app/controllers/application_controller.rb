fcb0ab64959fe21152d2af4d8c4ac972edfbea9f
Pierre-Etienne Soury <pesoury@ownpage.fr>
2019-03-06 17:58:53 +0100

    fixed question access to the public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
