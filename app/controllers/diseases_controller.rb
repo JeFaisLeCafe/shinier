993943c5e2dda5777e17c74cad96f08961801629
Pierre-Etienne Soury <pesoury@ownpage.fr>
2019-03-11 10:22:14 +0100

    fixing the :authenticate_user! before_action bug

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,5 +1,5 @@
 class DiseasesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show]
+  # skip_before_action :authenticate_user!, only: [:show]
 
   def show
     @disease = Disease.find(params[:id])
